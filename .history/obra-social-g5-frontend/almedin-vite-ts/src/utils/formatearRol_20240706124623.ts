357051a96dca65d2c703d2432c32e7de174c2efa
Mpablom <100524470+Mpablom@users.noreply.github.com>
2024-07-06 23:16:32 +0000

    elimiando .history

--- .history/obra-social-g5-frontend/almedin-vite-ts/src/utils/formatearRol_20240706124623.ts
+++
@@ -1,6 +0,0 @@
-import { UsuarioRolesEnum } from "../assets/models/Usuario";
-
-export function formatearRol(rol: UsuarioRolesEnum) {
-    const formatRol = rol.replace('ROL_', '').toLowerCase();
-    return formatRol;
-}
